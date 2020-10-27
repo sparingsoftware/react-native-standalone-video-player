b7718d2492afc6a4dccaffc14d4c882ba16a5576
Ufos <>
2020-10-27 19:01:50 +0100

    gifs

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -22,7 +22,7 @@ export default function App() {
     load(
       'https://bitdash-a.akamaihd.net/content/MI201109210084_1/m3u8s/f08e80da-bf1d-4e3d-8899-f0f6155f6efa.m3u8'
     );
-  }, [load]);
+  }, []);
 
   //
 
