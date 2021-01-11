ca72296b3468b7b2fb0c6a8df74bd9454f14b4f4
Ufos <>
2021-01-11 13:35:09 +0100

    Fix android native player view

--- android/src/main/java/com/reactnativestandalonevideoplayer/PlayerContainerView.kt
+++ android/src/main/java/com/reactnativestandalonevideoplayer/PlayerContainerView.kt
@@ -1,6 +1,7 @@
 package com.reactnativestandalonevideoplayer
 
 
+import android.content.Context
 import android.os.Looper
 import android.util.Log
 import android.view.SurfaceView
@@ -12,55 +13,69 @@ import com.google.android.exoplayer2.SimpleExoPlayer
 import com.google.android.exoplayer2.ui.AspectRatioFrameLayout
 import com.google.android.exoplayer2.ui.PlayerView
 
-class PlayerContainerView: SimpleViewManager<PlayerView>() {
 
-  private lateinit var playerView: PlayerView
+class MyPlayerView(context: Context): PlayerView(context) {
 
-  private var playerInstance: Int = 0
-  private var isBound: Boolean = false
+  var playerInstance: Int = -1
+  var isBound: Boolean = false
 
-  //
+}
+
+//
+
+class PlayerContainerView: SimpleViewManager<MyPlayerView>() {
+
+
+  init {
+    // this is created only once!
+    Log.d("PlayerView", "init PlayerContainerView")
+  }
 
   override fun getName() = "RNTPlayerVideoView"
 
   @ReactProp(name = "isBoundToPlayer")
-  fun boundToPlayer(view: PlayerView, isBoundToPlayer: Boolean) {
-    Log.d("PlayerView", "isBoundToPlayer = ${isBoundToPlayer}, playerInstance=${playerInstance}")
+  fun boundToPlayer(view: MyPlayerView, isBoundToPlayer: Boolean) {
+    Log.d("PlayerView", "boundToPlayer = ${isBoundToPlayer}")
 
-    isBound = isBoundToPlayer
+    view.isBound = isBoundToPlayer
 
-    bind(view, isBound)
+    setup(view)
   }
 
   @ReactProp(name = "playerInstance")
-  fun setPlayerInstance(view: PlayerView, instance: Int) {
+  fun setPlayerInstance(view: MyPlayerView, instance: Int) {
     Log.d("PlayerView", "playerInstance = ${instance}")
 
-    playerInstance = instance
+    view.playerInstance = instance
 
-    bind(view, isBound)
+    setup(view)
   }
 
-  private fun bind(view: PlayerView, isBound: Boolean) {
-    Log.d("PlayerView", "bind isBound=${isBound}, playerInstance=${playerInstance}")
+  private fun setup(view: MyPlayerView) {
+    Log.d("PlayerView", "bind isBound=${view.isBound}, playerInstance=${view.playerInstance}")
+    Log.d("PlayerView", "view = ${view}")
 
-    view.player = if (isBound && playerInstance >= 0) PlayerVideo.instances[playerInstance].player else null
+    if (view.playerInstance < 0 || view.playerInstance >= PlayerVideo.instances.size) {
+      return
+    }
+
+    view.player = if (view.isBound) PlayerVideo.instances[view.playerInstance].player else null
     view.resizeMode = AspectRatioFrameLayout.RESIZE_MODE_FILL
     (view.player as? SimpleExoPlayer)?.videoScalingMode = C.VIDEO_SCALING_MODE_SCALE_TO_FIT_WITH_CROPPING
-    
+
     //
-    if (isBound) {
+    if (view.isBound) {
       // we have to bind again after videoSizeChanged otherwise video ratio would be wrong
-      PlayerVideo.instances[playerInstance].videoSizeChanged = { width, height ->
-        bind(view, true)
+      PlayerVideo.instances[view.playerInstance].videoSizeChanged = { width, height ->
+//        bind(view, true)
       }
     }
   }
 
   //
 
-  override fun createViewInstance(reactContext: ThemedReactContext): PlayerView {
-    playerView = PlayerView(reactContext)
+  override fun createViewInstance(reactContext: ThemedReactContext): MyPlayerView {
+    val playerView = MyPlayerView(reactContext)
 
     playerView.useController = false
     playerView.resizeMode = AspectRatioFrameLayout.RESIZE_MODE_FILL
